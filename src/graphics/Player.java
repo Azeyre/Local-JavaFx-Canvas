a26637eff5705e5ddf4394ca84083385997beb58
Azeyre <31286257+Azeyre@users.noreply.github.com>
2019-04-26 17:10:14 +0200

    First Commit

---
+++ src/graphics/Player.java
@@ -0,0 +1,49 @@
+package graphics;
+
+import java.io.Serializable;
+
+public class Player implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3808620236824524793L;
+	private int id;
+	private double x,y;
+	private static final int SIZE = 20;
+
+	Player(int id, double x, double y){
+		this.x = x;
+		this.y = y;
+		this.id = id;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public int getSize() {
+		return SIZE;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String toString() {
+		return "Id : " + id + " :[x:" + x + ", y:" + y + "]";
+	}
+	
+}
